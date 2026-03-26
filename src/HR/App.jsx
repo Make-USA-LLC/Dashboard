61c46c02f1216b0efabce46160670684439ecbe6
danielsmakeusa <daniel.s@makeit.buzz>
2026-03-26 18:05:42 +0200

    fixed capital. letter

--- src/HR/App.jsx
+++ src/HR/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
 import { useRole, RoleProvider } from './hooks/useRole'; 
 import './HR.css'; 
-import Loader from '../components/Loader';
+import Loader from '../components/loader';
 
 // Page Imports
 import Dashboard from './pages/Dashboard';
