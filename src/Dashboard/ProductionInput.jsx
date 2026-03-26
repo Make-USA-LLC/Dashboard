7a7e70e91a9d079256152ac85b3783cd6a1241ab
danielsmakeusa <daniel.s@makeit.buzz>
2026-03-26 18:28:22 +0200

    Update ProductionInput.jsx

--- src/Dashboard/ProductionInput.jsx
+++ src/Dashboard/ProductionInput.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ProductionInput.css';
-import Loader from '../components/loader';
+import Loader from '../components/Loader';
 import { db } from './firebase_config.jsx';
 import { collection, query, orderBy, limit, getDocs, doc, getDoc, updateDoc, deleteDoc, addDoc, serverTimestamp } from 'firebase/firestore';
 import { useRole } from './hooks/useRole'; // <-- Imported centralized hook
